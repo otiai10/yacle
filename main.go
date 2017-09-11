e0ae961153f0f35d6ec7a99ab2d8086448eabc82
Hiromu OCHIAI <otiai10@gmail.com>
2017-09-11 17:16:21 +0900

    Add "outdir"

--- main.go
+++ main.go
@@ -15,6 +15,16 @@ func main() {
 	app.Commands = []cli.Command{
 		commands.Run, // yacle run
 	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "outdir",
+			Usage: "Output Directory",
+		},
+		cli.BoolFlag{
+			Name:  "quiet",
+			Usage: "compress stdout log",
+		},
+	}
 	app.Action = commands.Run.Action // yacle (without subcommand)
 	if err := app.Run(os.Args); err != nil {
 		log.Fatalln(err)
