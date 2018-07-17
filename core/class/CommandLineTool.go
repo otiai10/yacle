0df6b6f148a3ac1c8a2f9450044a6a5e4f084da7
Hiromu OCHIAI <otiai10@gmail.com>
2018-07-18 01:11:13 +0900

    Clean up some code

--- core/class/CommandLineTool.go
+++ core/class/CommandLineTool.go
@@ -31,289 +31,35 @@ func (tool *CommandLineTool) Run() error {
 		return fmt.Errorf("failed to ensure required inputs: %v", err)
 	}
 
-	cmd, err := tool.generateCommand(priors, arguments, inputs)
+	cmd, err := tool.generateBasicCommand(priors, arguments, inputs)
 	if err != nil {
 		return fmt.Errorf("failed to generate command struct: %v", err)
 	}
 	tool.Command = cmd
 
-	if err := tool.cmdが実行されるディレクトリを決める(); err != nil {
-		return fmt.Errorf("cmdが実行されるディレクトリを決める: %v", err)
+	if err := tool.defineCommandExecDirectory(); err != nil {
+		return fmt.Errorf("failed to define command execution directory: %v", err)
 	}
 
-	if err := tool.標準出力の行き先を決める(); err != nil {
-		return fmt.Errorf("標準出力の行き先を決める: %v", err)
+	if err := tool.defineStdoutDestination(); err != nil {
+		return fmt.Errorf("failed to define stdout destination: %v", err)
 	}
 
-	if err := tool.標準エラーの行き先を決める(); err != nil {
-		return fmt.Errorf("標準エラーの行き先を決める: %v", err)
+	if err := tool.defineStderrDestination(); err != nil {
+		return fmt.Errorf("failed to define stderr destination: %v", err)
 	}
 
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("exec failed: %v", err)
 	}
 
-	if err := tool.outdirを決定する(); err != nil {
-		return fmt.Errorf("outdirを決定する: %v", err)
+	if err := tool.defineOutputDir(); err != nil {
+		return fmt.Errorf("failed to define output dir: %v", err)
 	}
 
-	if err := tool.outdirにいろいろ揃える(); err != nil {
-		return fmt.Errorf("outdirにいろいろ揃える: %v", err)
+	if err := tool.arrangeOutputDirContents(); err != nil {
+		return fmt.Errorf("failed to arrange output contents: %v", err)
 	}
-
-	return nil
-
-	// // Set command execution directory
-	// commandExecDir := tool.Root.Path
-	// cmd.Dir = filepath.Dir(commandExecDir)
-	// if tool.Root.Outputs[0].Types[0].Type == "Directory" {
-	// 	// Create TempDir for exec
-	// 	commandExecDir, _ = ioutil.TempDir("/tmp", "yacleexec")
-	// 	srcName := filepath.Join(filepath.Dir(tool.Root.Path), inputs[0])
-	// 	dstName := filepath.Join(commandExecDir, inputs[0])
-
-	// 	// Create copied file
-	// 	os.Link(srcName, dstName)
-	// 	cmd.Dir = commandExecDir
-	// }
-	// if tool.Root.Outputs[0].Types[0].Type != "File" && tool.Root.Outputs[0].Types[0].Type != "int" && tool.Root.Outputs[0].Types[0].Type != "stdout" && tool.Root.Outputs[0].Types[0].Type != "stderr" && tool.Root.Outputs[0].Types[0].Type != "Directory" {
-	// 	if err := cmd.Run(); err != nil {
-	// 		return fmt.Errorf("failed to execute BaseCommand: %v", err)
-	// 	}
-	// }
-	// var filename string
-	// isStdoutFlag := false
-	// isStderrFlag := false
-	// stderrFilename := tool.Root.Stderr
-	// for i := range tool.Root.Outputs {
-	// 	switch tool.Root.Outputs[i].Types[0].Type {
-	// 	case "stderr":
-	// 		stderrFilename = tool.Root.Outputs[i].ID
-	// 		isStderrFlag = true
-	// 	case "stdout":
-	// 		filename = tool.Root.Outputs[i].ID
-	// 		isStdoutFlag = true
-	// 	}
-	// }
-	// if tool.Root.Stdout != "" {
-	// 	filename = tool.Root.Stdout
-	// 	isStdoutFlag = true
-	// }
-	// if tool.Root.Stderr != "" {
-	// 	stderrFilename = tool.Root.Stderr
-	// 	isStderrFlag = true
-	// }
-	// // TODO check filename
-	// if filename == "" {
-	// 	filename = stderrFilename
-	// 	if stderrFilename == "" {
-	// 		letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	// 		filenamelen := 16
-	// 		randombytearray := make([]byte, filenamelen)
-	// 		rand.Seed(time.Now().UnixNano())
-	// 		for i := range randombytearray {
-	// 			randombytearray[i] = letters[rand.Intn(len(letters))]
-	// 		}
-	// 		filename = string(randombytearray)
-	// 		// TODO only accept
-	// 		isStdoutFlag = true
-	// 	}
-	// }
-
-	// // {{{ TODO: Remove this hard coding!!
-	// if output, err := os.Open(filepath.Join(filepath.Dir(commandExecDir), "cwl.output.json")); err == nil {
-	// 	defer output.Close()
-	// 	if _, err := io.Copy(os.Stdout, output); err != nil {
-	// 		return fmt.Errorf("failed to dump standard output file: %v", err)
-	// 	}
-	// 	if err := os.Rename(output.Name(), filepath.Join(tool.Outdir, filepath.Base(output.Name()))); err != nil {
-	// 		return fmt.Errorf("failed to move starndard output file: %v", err)
-	// 	}
-	// 	return nil
-	// }
-
-	// if tool.Root.Outputs[0].Types[0].Type == "File" || tool.Root.Outputs[0].Types[0].Type == "int" || tool.Root.Outputs[0].Types[0].Type == "stdout" || tool.Root.Outputs[0].Types[0].Type == "stderr" || tool.Root.Outputs[0].Types[0].Type == "Directory" {
-	// 	var output *os.File
-	// 	var err error
-	// 	if isStdoutFlag {
-	// 		output, err = os.Create(filepath.Join(filepath.Dir(commandExecDir), filename))
-	// 	}
-	// 	var stderrOutput *os.File
-	// 	var stderrErr error
-	// 	if isStderrFlag {
-	// 		stderrOutput, stderrErr = os.Create(filepath.Join(filepath.Dir(commandExecDir), stderrFilename))
-	// 	} else {
-	// 		stderrOutput = output
-	// 	}
-	// 	if err == nil {
-	// 		defer output.Close()
-	// 	}
-	// 	if stderrErr == nil {
-	// 		defer stderrOutput.Close()
-	// 	}
-	// 	if err == nil {
-	// 		cmd.Stdout = output
-	// 		cmd.Stderr = stderrOutput
-	// 		vm := otto.New()
-	// 		inputs, _ := vm.Object(`inputs = {}`)
-	// 		file1, _ := vm.Object(`file1 = {}`)
-	// 		inputs.Set("file1", file1)
-	// 		file1.Set("path", "whale.txt")
-	// 		stdinFile, _ := vm.Run("inputs.file1.path")
-	// 		sF, _ := os.Open(filepath.Join(filepath.Dir(commandExecDir) + "/" + stdinFile.String()))
-	// 		stdin, errStdin := cmd.StdinPipe()
-	// 		if errStdin != nil {
-	// 			fmt.Println(errStdin)
-	// 		}
-
-	// 		io.Copy(stdin, bufio.NewReader(sF))
-	// 		stdin.Close()
-	// 		if err := cmd.Run(); err != nil {
-	// 			return err
-	// 		}
-	// 		// if _, err := io.Copy(os.Stdout, output); err != nil {
-	// 		// 	return fmt.Errorf("failed to dump standard output file: %v", err)
-	// 		// }
-	// 		if isStdoutFlag {
-	// 			if err := os.Rename(output.Name(), filepath.Join(tool.Outdir, filepath.Base(output.Name()))); err != nil {
-	// 				return fmt.Errorf("failed to move starndard output file: %v", err)
-	// 			}
-	// 		}
-	// 		if isStderrFlag {
-	// 			if stderrErr := os.Rename(stderrOutput.Name(), filepath.Join(tool.Outdir, filepath.Base(stderrOutput.Name()))); stderrErr != nil {
-	// 				return fmt.Errorf("failed to move starndard error file: %v", err)
-	// 			}
-	// 		}
-	// 	}
-
-	// 	// TODO output file information
-	// 	// This is for n7
-	// 	// n9 requires extend here.
-	// 	// before this part, we need to create given filename
-	// 	if tool.Root.Outputs[0].Types[0].Type != "Directory" &&
-	// 		!(isStdoutFlag == true && isStderrFlag == true) {
-	// 		checksum := ""
-	// 		path := ""
-	// 		basename := ""
-	// 		location := ""
-	// 		var size int64 = -1
-	// 		if f, err := os.Open(filepath.Join(tool.Outdir, filepath.Base(filename))); err == nil {
-	// 			path = f.Name()
-	// 			basename = filepath.Base(path)
-	// 			location = fmt.Sprintf("file://%s", path)
-
-	// 			defer f.Close()
-	// 			fileinfo, _ := f.Stat()
-	// 			size = fileinfo.Size()
-	// 			h := sha1.New()
-	// 			if _, err := io.Copy(h, f); err != nil {
-	// 				log.Fatal(err)
-	// 			}
-
-	// 			checksum = fmt.Sprintf("sha1$%x", string(h.Sum(nil)))
-	// 		}
-	// 		outputIdentifier := tool.Root.Outputs[0].ID
-	// 		if tool.Root.Outputs[0].Binding != nil && tool.Root.Outputs[0].Binding.LoadContents {
-	// 			if f2, err := os.Open(filepath.Join(tool.Outdir, filepath.Base(filename))); err == nil {
-	// 				defer f2.Close()
-	// 				lr := io.LimitReader(f2, 64*1024)
-	// 				buf := new(bytes.Buffer)
-	// 				buf.ReadFrom(lr)
-	// 				s := buf.String()
-	// 				vm2 := otto.New()
-	// 				self := []map[string]interface{}{
-	// 					{
-	// 						"contents": s,
-	// 					},
-	// 				}
-	// 				vm2.Set("self", self)
-	// 				//
-	// 				contentsBytes := []byte(tool.Root.Outputs[0].Binding.Eval)
-	// 				contents := string(contentsBytes[2 : len(contentsBytes)-1])
-	// 				resultObject, _ := vm2.Run(contents)
-	// 				result, _ := resultObject.ToString()
-	// 				fmt.Println("{\"" + outputIdentifier + "\": " + result + "}")
-	// 			}
-	// 		} else {
-	// 			fmt.Println("{\"" + outputIdentifier + "\":{\"checksum\": \"" + checksum + "\",\"basename\": \"" + basename + "\",\"location\": \"" + location + "\",\"path\": \"" + path + "\",\"class\": \"File\",\"size\": " + strconv.FormatInt(size, 10) + "}}")
-	// 		}
-	// 	} else {
-	// 		if isStdoutFlag == true && isStderrFlag == true {
-	// 			files, err := ioutil.ReadDir(tool.Outdir)
-	// 			if err != nil {
-	// 				panic(err)
-	// 			}
-	// 			listingString := ""
-	// 			for _, file := range files {
-	// 				checksum := ""
-	// 				path := ""
-	// 				basename := ""
-	// 				var size int64 = -1
-	// 				if f, err := os.Open(tool.Outdir + "/" + file.Name()); err == nil {
-	// 					path = f.Name()
-	// 					basename = filepath.Base(path)
-
-	// 					defer f.Close()
-	// 					fileinfo, _ := f.Stat()
-	// 					size = fileinfo.Size()
-	// 					h := sha1.New()
-	// 					if _, err := io.Copy(h, f); err != nil {
-	// 						log.Fatal(err)
-	// 					}
-
-	// 					checksum = fmt.Sprintf("sha1$%x", string(h.Sum(nil)))
-	// 					if listingString != "" {
-	// 						listingString = listingString + ","
-	// 					}
-	// 					listingString = listingString + "\"" + basename + "\":{\"checksum\": \"" + checksum + "\",\"location\": \"" + "Any" + "\",\"class\": \"File\",\"size\": " + strconv.FormatInt(size, 10) + "}"
-	// 				}
-	// 			}
-	// 			fmt.Println("{" + listingString + "}")
-	// 		} else if tool.Root.Outputs[0].Binding.Glob[0] == "." {
-	// 			// Remove linked input file
-	// 			dstName := filepath.Join(commandExecDir, inputs[0])
-	// 			errRemove := os.Remove(dstName)
-
-	// 			if errRemove != nil {
-	// 				panic(errRemove)
-	// 			}
-	// 			files, err := ioutil.ReadDir(commandExecDir)
-	// 			if err != nil {
-	// 				panic(err)
-	// 			}
-	// 			listingString := ""
-	// 			for _, file := range files {
-	// 				checksum := ""
-	// 				path := ""
-	// 				basename := ""
-	// 				location := ""
-	// 				var size int64 = -1
-	// 				if f, err := os.Open(commandExecDir + "/" + file.Name()); err == nil {
-	// 					path = f.Name()
-	// 					basename = filepath.Base(path)
-	// 					location = fmt.Sprintf("file://%s", path)
-
-	// 					defer f.Close()
-	// 					fileinfo, _ := f.Stat()
-	// 					size = fileinfo.Size()
-	// 					h := sha1.New()
-	// 					if _, err := io.Copy(h, f); err != nil {
-	// 						log.Fatal(err)
-	// 					}
-
-	// 					checksum = fmt.Sprintf("sha1$%x", string(h.Sum(nil)))
-	// 					if listingString != "" {
-	// 						listingString = listingString + ","
-	// 					}
-	// 					listingString = listingString + "{\"checksum\": \"" + checksum + "\",\"basename\": \"" + basename + "\",\"location\": \"" + location + "\",\"path\": \"" + path + "\",\"class\": \"File\",\"size\": " + strconv.FormatInt(size, 10) + "}"
-	// 				}
-	// 			}
-	// 			outputIdentifier := tool.Root.Outputs[0].ID
-	// 			fmt.Println("{\"" + outputIdentifier + "\":{\"class\": \"Directory\",\"listing\": [" + listingString + "]}}")
-	// 		}
-	// 	}
-	// }
-	// // }}}
 
 	return nil
 }
@@ -386,8 +132,8 @@ func (tool *CommandLineTool) AliasFor(key string) string {
 	return ""
 }
 
-// generateCommand ...
-func (tool *CommandLineTool) generateCommand(priors, arguments, inputs []string) (*exec.Cmd, error) {
+// generateBasicCommand ...
+func (tool *CommandLineTool) generateBasicCommand(priors, arguments, inputs []string) (*exec.Cmd, error) {
 
 	if len(tool.Root.BaseCommands) == 0 {
 		return exec.Command("bash", "-c", tool.Root.Arguments[0].Binding.ValueFrom.Key()), nil
@@ -403,9 +149,8 @@ func (tool *CommandLineTool) generateCommand(priors, arguments, inputs []string)
 	return exec.Command(oneline[0], oneline[1:]...), nil
 }
 
-// cmdが実行されるディレクトリを決める
-// tool.Commandが生成されている必要がある
-func (tool *CommandLineTool) cmdが実行されるディレクトリを決める() error {
+// defineCommandExecDirectory
+func (tool *CommandLineTool) defineCommandExecDirectory() error {
 
 	rootdir := filepath.Dir(tool.Root.Path)
 	tool.Command.Dir = rootdir
@@ -427,9 +172,8 @@ func (tool *CommandLineTool) cmdが実行されるディレクトリを決める
 	return nil
 }
 
-// 標準出力の行き先を決める
-// tool.Command、およびtool.Command.Dirが先に決定されている必要がある
-func (tool *CommandLineTool) 標準出力の行き先を決める() error {
+// defineStdoutDestination
+func (tool *CommandLineTool) defineStdoutDestination() error {
 
 	// Prefer "stdout" specified on ROOT
 	if tool.Root.Stdout != "" {
@@ -458,8 +202,8 @@ func (tool *CommandLineTool) 標準出力の行き先を決める() error {
 	return nil
 }
 
-// 標準エラーの行き先を決める
-func (tool *CommandLineTool) 標準エラーの行き先を決める() error {
+// defineStderrDestination
+func (tool *CommandLineTool) defineStderrDestination() error {
 
 	// Prefer "stderr" specified on ROOT
 	if tool.Root.Stderr != "" {
@@ -487,8 +231,8 @@ func (tool *CommandLineTool) 標準エラーの行き先を決める() error {
 	return nil
 }
 
-// outdirを決定する
-func (tool *CommandLineTool) outdirを決定する() error {
+// defineOutputDir
+func (tool *CommandLineTool) defineOutputDir() error {
 	if tool.Outdir != "" {
 		return nil
 	}
@@ -497,8 +241,8 @@ func (tool *CommandLineTool) outdirを決定する() error {
 	return nil
 }
 
-// outdirにいろいろ揃える。必要があればcwl.output.jsonをつくる
-func (tool *CommandLineTool) outdirにいろいろ揃える() error {
+// arrangeOutputDirContents
+func (tool *CommandLineTool) arrangeOutputDirContents() error {
 
 	// If "cwl.output.json" exists on executed command directory,
 	// dump the file contents on stdout.
