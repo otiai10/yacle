25985285e08ec2b2e37d06164ca00aa6a049f6a6
Hiromu OCHIAI <otiai10@gmail.com>
2018-07-18 18:00:04 +0900

    Pass No.11,12

--- core/class/CommandLineTool.go
+++ core/class/CommandLineTool.go
@@ -272,7 +272,7 @@ func (tool *CommandLineTool) arrangeOutputDirContents() error {
 	// CWL wants to dump metadata of outputs with type="File"
 	// See also https://www.commonwl.org/v1.0/CommandLineTool.html#File
 	for _, output := range tool.Root.Outputs {
-		if err := output.DumpFileMeta(tool.Command.Dir, tool.Root.Stdout, os.Stdout); err != nil {
+		if err := output.DumpFileMeta(tool.Command.Dir, tool.Root.Stdout, tool.Root.Stderr, os.Stdout); err != nil {
 			return err
 		}
 	}
